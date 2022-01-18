11a8626cf9f69815495c359e2c3d84722df2168f
Akihiro.TOYOSHIMA <cabstation_ccd@kabushikikaishakyabusuteshonnoMacBook-Air.local>
2022-01-18 16:32:12 +0900

    fix

--- laravel_sample/app/Http/Kernel.php
+++ laravel_sample/app/Http/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends HttpKernel
     /**
      * The application's route middleware groups.
      *
-     * @var array
+     * @var array1
      */
     protected $middlewareGroups = [
         'web' => [
