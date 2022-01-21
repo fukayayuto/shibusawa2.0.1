a02f70e3857ea1e76c91da001550d213360c9d34
Akihiro.TOYOSHIMA <cabstation_ccd@kabushikikaishakyabusuteshonnoMacBook-Air.local>
2022-01-21 11:30:44 +0900

    fix

--- laravel_sample/app/Http/Kernel.php
+++ laravel_sample/app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            // \App\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
 
             // \App\Http\Middleware\EncryptCookies::class,
@@ -54,17 +54,16 @@ class Kernel extends HttpKernel
 
         'api' => [
             // 'throttle:60,1',
+       
+            'throttle:60,1',
+            'bindings',
+     
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            'throttle:60,1',
-            'bindings',
-        //    \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        //    \App\Http\Middleware\EncryptCookies::class,
-        //    \Illuminate\Session\Middleware\StartSession::class,
-        //    \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        //    \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
 
