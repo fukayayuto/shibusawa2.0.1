b2f50d0bd2ba1370f9f0c26fec15e5089dbe0ce0
Akihiro.TOYOSHIMA <cabstation_ccd@kabushikikaishakyabusuteshonnoMacBook-Air.local>
2022-01-21 10:56:29 +0900

    fix

--- laravel_sample/app/Http/Kernel.php
+++ laravel_sample/app/Http/Kernel.php
@@ -55,6 +55,9 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            \App\Http\Middleware\EncryptCookies::class,
+     +      \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+     +      \Illuminate\Session\Middleware\StartSession::class,
         ],
     ];
 
