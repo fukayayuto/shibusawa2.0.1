1f8857669a6c280d2578c3afe0183fc11ae3f684
Akihiro.TOYOSHIMA <cabstation_ccd@kabushikikaishakyabusuteshonnoMacBook-Air.local>
2022-01-21 11:19:44 +0900

    fix

--- laravel_sample/app/Http/Kernel.php
+++ laravel_sample/app/Http/Kernel.php
@@ -53,13 +53,18 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            // 'throttle:60,1',
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             'throttle:60,1',
-            
-           \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-           \App\Http\Middleware\EncryptCookies::class,
-           \Illuminate\Session\Middleware\StartSession::class,
-           \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-           \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'bindings',
+        //    \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        //    \App\Http\Middleware\EncryptCookies::class,
+        //    \Illuminate\Session\Middleware\StartSession::class,
+        //    \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        //    \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
 
